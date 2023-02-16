68270e49b5879ad8d8b3d6eab886cf47b63ad23c
LaraSharmon <113535701+LaraSharmon@users.noreply.github.com>
2023-02-16 11:38:03 -0300

    adição da rota de usuário

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+
+const conf = require('dotenv').config().parsed;
+
+console.log(conf);
+
+const port = conf.HTTPPORT;
+
+// Objeto que será executado quando houver uma requisição.
+const thing = {
+  getAll: async (req, res) => {
+    res.json({ "req": req.method, "id": req.params.id, "status": "ok" });
+  },
+  getOne: async (req, res) => {
+    const id = req.params.id;
+    res.json({ "req": req.method, "id": id, "status": "ok" });
+  },
+  post: async (req, res) => {
+    res.json({ "req": req.method, "status": "ok" });
+  },
+  put: async (req, res) => {
+    const id = req.params.id;
+    res.json({ "req": req.method, "id": id, "status": "ok" });
+  },
+  delete: async (req, res) => {
+    const id = req.params.id;
+    res.json({ "req": req.method, "id": id, "status": "ok" });
+  }
+}
+// Objeto que trata requisições para o 'user'.
+const user = {
+  getOne: async (req, res) => { },
+  post: async (req, res) => { },
+  put: async (req, res) => { },
+  delete: async (req, res) => { }
+}
+
+// Recebe os dados do body HTTP e valida em JSON → POST e PUT.
+const bodyParser = require('body-parser').json();
+
+// Rota para GET → getAll() → Recebe, por exemplo, todos os registros.
+app.get('/', thing.getAll);
+
+// Rota para GET → get(id) → Recebe apenas o registro identificado.
+app.get('/:id', thing.getOne);
+
+// Rota para DELETE.
+app.delete('/:id', thing.delete);
+
+// Rota para POST → bodyParser (no hook) é usado para garantir a chegada de um JSON.
+app.post('/', bodyParser, thing.post);
+
+// Rota para PUT → bodyParser (no hook) é usado para garantir a chegada de um JSON.
+app.put('/:id', bodyParser, thing.put);
+
+// rotas para o usuário.
+app.get('/user/:id', thing.userGetOne);
+app.put('/user/:id', thing.userPut);
+app.delete('/user/:id', thing.userDelete);
+
+// Colocar comentario.
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
